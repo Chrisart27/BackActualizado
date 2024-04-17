11d551bd763cd94ff37a13cecfb414632fb03405
christianbohorquez <arturobohorquez@gmail.com>
2024-04-17 02:29:06 -0500

    Se agregaron la latitud y longitud a las gruas

--- index.js
+++ index.js
@@ -1,66 +1,69 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const signingKey = require('./src/config/keys');
-const auth = require('./src/routes/getJwt');
-const login = require('./src/routes/login');
-const register = require('./src/routes/register');
-const gruasRoutes = require('./src/routes/gruasRutes');
-const getGruasInfoRoute = require('./src/routes/getGruasInfo');
-const editarGruaRouter = require('./src/routes/editarGruasRouter'); // Agregado
-const eliminarGruaRouter = require('./src/routes/eliminarGruasRouter'); // Agregado
-const consultarGruasId = require('./src/routes/consultarGruaIdRouter'); // Agregado
-const editarUser = require('./src/routes/editarInfoUser'); // Agregado
-const getToken = require('./src/routes/getJwt'); // Agregado
-const sendEmail = require('./src/routes/email'); // Agregado
-const cambiarEstadoGrua = require('./src/routes/cambiarEstadoGrua'); // Agregado
-const obtenerGruasPendientes = require('./src/routes/getGruasPendientes'); // Agregado
-const loginAdmin = require('./src/routes/loginAdmin'); // Agregado
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const signingKey = require("./src/config/keys");
+const auth = require("./src/routes/getJwt");
+const login = require("./src/routes/login");
+const register = require("./src/routes/register");
+const gruasRoutes = require("./src/routes/gruasRutes");
+const getGruasInfoRoute = require("./src/routes/getGruasInfo");
+const editarGruaRouter = require("./src/routes/editarGruasRouter"); // Agregado
+const eliminarGruaRouter = require("./src/routes/eliminarGruasRouter"); // Agregado
+const consultarGruasId = require("./src/routes/consultarGruaIdRouter"); // Agregado
+const editarUser = require("./src/routes/editarInfoUser"); // Agregado
+const getToken = require("./src/routes/getJwt"); // Agregado
+const sendEmail = require("./src/routes/email"); // Agregado
+const cambiarEstadoGrua = require("./src/routes/cambiarEstadoGrua"); // Agregado
+const obtenerGruasPendientes = require("./src/routes/getGruasPendientes"); // Agregado
+const loginAdmin = require("./src/routes/loginAdmin"); // Agregado
+const cors = require("cors");
 
-const app = express().use(cors()).use(bodyParser.json()).use(cookieParser(signingKey.SIGNING_KEY_COOKIE));
+const app = express()
+  .use(cors())
+  .use(bodyParser.json())
+  .use(cookieParser(signingKey.SIGNING_KEY_COOKIE));
 
-const { API_KEY_GEMINI, START_CHAT, GENERATION_CONFIG } = require('./config');
+const { API_KEY_GEMINI, START_CHAT, GENERATION_CONFIG } = require("./config");
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(API_KEY_GEMINI);
 const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
 // Establece la ruta base de las imágenes
-app.locals.rutaBaseImagenes = '/ruta-base-imagenes'; // Cambia esto según tus necesidades
+app.locals.rutaBaseImagenes = "/ruta-base-imagenes"; // Cambia esto según tus necesidades
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
 
-app.use('/register', register);
-app.use('/login', login);
-app.use('/gruas', gruasRoutes);
-app.use('/getGruasInfo', getGruasInfoRoute);
-app.use('/editarGrua', editarGruaRouter); // Agregado
-app.use('/eliminarGrua', eliminarGruaRouter); // Agregado
-app.use('/gruasClient', consultarGruasId); // Agregado
-app.use('/editarUser', editarUser); // Agregado
-app.use('/getToken', getToken); // Agregado
-app.use('/sendEmail', sendEmail); // Agregado
-app.use('/cambiarEstadoGrua', cambiarEstadoGrua); // Agregado
-app.use('/getGruasPendiente', obtenerGruasPendientes); // Agregado
-app.use('/loginAdmin', loginAdmin); // Agregado
+app.use("/register", register);
+app.use("/login", login);
+app.use("/gruas", gruasRoutes);
+app.use("/getGruasInfo", getGruasInfoRoute);
+app.use("/editarGrua", editarGruaRouter); // Agregado
+app.use("/eliminarGrua", eliminarGruaRouter); // Agregado
+app.use("/gruasClient", consultarGruasId); // Agregado
+app.use("/editarUser", editarUser); // Agregado
+app.use("/getToken", getToken); // Agregado
+app.use("/sendEmail", sendEmail); // Agregado
+app.use("/cambiarEstadoGrua", cambiarEstadoGrua); // Agregado
+app.use("/getGruasPendiente", obtenerGruasPendientes); // Agregado
+app.use("/loginAdmin", loginAdmin); // Agregado
 
 const PORT = process.env.PORT ?? 3000;
 
-app.get('/chat', (req, res) => {
-  res.send('GET request to /api/chat');
+app.get("/chat", (req, res) => {
+  res.send("GET request to /api/chat");
 });
 
-app.post('/chat', async (req, res) => {
+app.post("/chat", async (req, res) => {
   let history = req.body.history;
   let question = req.body.question;
-  let historyChat = START_CHAT.concat(history)
+  let historyChat = START_CHAT.concat(history);
   const chat = model.startChat({
-      history: historyChat,
-      generationConfig: GENERATION_CONFIG
+    history: historyChat,
+    generationConfig: GENERATION_CONFIG,
   });
   const sendQuestion = await chat.sendMessage(question);
   const response = await sendQuestion.response;
@@ -70,6 +73,45 @@ app.post('/chat', async (req, res) => {
   return res.status(200).json({ history: history });
 });
 
-app.listen(PORT, () => {
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173", //by writing an origin like this,
+    // server will only allow requests from localhost:5173
+  },
+});
+
+let usuariosEnLinea = [];
+
+io.on("connection", (socket) => {
+  // Envia todos los usuarios en linea a todos los usuarios
+  io.emit("obtener-usuarios", JSON.stringify(usuariosEnLinea));
+  // Nuevo usuario agregado
+  socket.on("nuevo-usuario-agregado", (nuevoIdCliente) => {
+    if (
+      !usuariosEnLinea.some((usuario) => usuario.idCliente === nuevoIdCliente)
+    ) {
+      // Si el usuario no se ha agregado antes
+      usuariosEnLinea.push({ idCliente: nuevoIdCliente, socketId: socket.id });
+      console.log("Hay un nuevo usuario", usuariosEnLinea);
+    }
+    // Envia todos los usuarios en linea a todos los usuarios
+    io.emit("obtener-usuarios", JSON.stringify(usuariosEnLinea));
+  });
+
+  socket.on("usuario-desconectado", () => {
+    // Remueve el usuario de los usuarios activos
+    usuariosEnLinea = usuariosEnLinea.filter(
+      (usuario) => usuario.socketId !== socket.id
+    );
+    console.log("usuario esta desconecatdo", usuariosEnLinea);
+    // Envia todos los usuarios en linea a todos los usuarios
+    io.emit("obtener-usuarios", JSON.stringify(usuariosEnLinea));
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
 });
--- src/data/datas.js
+++ src/data/datas.js
@@ -1,40 +1,53 @@
-const mysql = require('mysql2');
-const path = require('path');
-const fs = require('fs');
+const mysql = require("mysql2");
+const path = require("path");
+const fs = require("fs");
 
 const database = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'GruappDta',
+  host: "localhost",
+  user: "root",
+  password: "admin123456789",
+  database: "GruappDta",
 });
 
 class userData {
-
   static addClient(cliente, callback) {
-    const query = "INSERT INTO cliente (nombre, email, contrasenia, telefono, idRol) VALUES (?, ?, ?, ?, ?)";
-    database.query(query, [cliente.nombre, cliente.email, cliente.contrasenia, cliente.telefono, 2], (err, result) => {
-      if (err) {
-        console.error("Error al registrar el cliente: " + err.message);
-        callback(err, false);
-      } else {
-        const nuevoClienteId = result && result.insertId;
-        console.log("Cliente registrado exitosamente. ID: " + nuevoClienteId);
-        callback(null, true, nuevoClienteId);
+    const query =
+      "INSERT INTO cliente (nombre, email, contrasenia, telefono, idRol) VALUES (?, ?, ?, ?, ?)";
+    database.query(
+      query,
+      [cliente.nombre, cliente.email, cliente.contrasenia, cliente.telefono, 2],
+      (err, result) => {
+        if (err) {
+          console.error("Error al registrar el cliente: " + err.message);
+          callback(err, false);
+        } else {
+          const nuevoClienteId = result && result.insertId;
+          console.log("Cliente registrado exitosamente. ID: " + nuevoClienteId);
+          callback(null, true, nuevoClienteId);
+        }
       }
-    });
+    );
   }
 
   static editarInformacionUsuario(idCliente, nuevosDetalles, callback) {
-    const query = 'UPDATE cliente SET nombre = ?, email = ?, telefono = ? WHERE idCliente = ?';
-    const values = [nuevosDetalles.nombre, nuevosDetalles.email, nuevosDetalles.telefono, idCliente];
-  
+    const query =
+      "UPDATE cliente SET nombre = ?, email = ?, telefono = ? WHERE idCliente = ?";
+    const values = [
+      nuevosDetalles.nombre,
+      nuevosDetalles.email,
+      nuevosDetalles.telefono,
+      idCliente,
+    ];
+
     database.query(query, values, (err, result) => {
       if (err) {
-        console.error('Error al editar la información del usuario:', err.message);
+        console.error(
+          "Error al editar la información del usuario:",
+          err.message
+        );
         callback(err, false);
       } else {
-        console.log('Información del usuario editada exitosamente.');
+        console.log("Información del usuario editada exitosamente.");
         callback(null, true);
       }
     });
@@ -42,26 +55,41 @@ class userData {
 
   static addGrua(grua, callback) {
     console.log(grua);
-    const query = "INSERT INTO grua (marca, modelo, capacidad, whatsapp, ubicacion, foto, estadoGrua, idCliente, idAdmin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-    const values = [grua.marca, grua.modelo, grua.capacidad, grua.whatsapp, grua.ubicacion, grua.foto, false, grua.idCliente, 1];
-  
+    const query =
+      "INSERT INTO grua (marca, modelo, capacidad, whatsapp, ubicacion, foto, estadoGrua, idCliente, idAdmin, latitud, longitud) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+    const values = [
+      grua.marca,
+      grua.modelo,
+      grua.capacidad,
+      grua.whatsapp,
+      grua.ubicacion,
+      grua.foto,
+      false,
+      grua.idCliente,
+      1,
+      grua.latitud,
+      grua.longitud,
+    ];
+
     database.query(query, values, (err, result) => {
       if (err) {
         console.error("Error al agregar la grúa 2: " + err.message);
-        return callback(err, false);  // Enviar false en caso de error
+        return callback(err, false); // Enviar false en caso de error
       } else {
         const nuevaGruaId = result && result.insertId;
         console.log("Grúa agregada exitosamente. ID: " + nuevaGruaId);
-        return callback(null, true, nuevaGruaId);  // Enviar true y la nuevaGruaId en caso de éxito
+        return callback(null, true, nuevaGruaId); // Enviar true y la nuevaGruaId en caso de éxito
       }
     });
   }
 
   static obtenerInformacionUsuario(idCliente, callback) {
-    const query = 'SELECT * FROM cliente WHERE idCliente = ?';
+    const query = "SELECT * FROM cliente WHERE idCliente = ?";
     database.query(query, [idCliente], (err, result) => {
       if (err) {
-        console.error('Error al obtener la información del usuario: ' + err.message);
+        console.error(
+          "Error al obtener la información del usuario: " + err.message
+        );
         callback(err, null);
       } else if (result.length === 0) {
         callback(null, null);
@@ -73,10 +101,10 @@ class userData {
   }
 
   static login(user, callback) {
-    const query = 'SELECT * FROM cliente WHERE email = ? AND contrasenia = ?';
+    const query = "SELECT * FROM cliente WHERE email = ? AND contrasenia = ?";
     database.query(query, [user.email, user.contrasenia], (err, result) => {
       if (err) {
-        console.error('Error al autenticar el usuario: ' + err.message);
+        console.error("Error al autenticar el usuario: " + err.message);
         callback(err, null);
       } else if (result.length === 0) {
         callback(null, null);
@@ -88,10 +116,10 @@ class userData {
   }
 
   static loginAdmin(user, callback) {
-    const query = 'SELECT * FROM admin WHERE email = ? AND contrasenia = ?';
+    const query = "SELECT * FROM admin WHERE email = ? AND contrasenia = ?";
     database.query(query, [user.email, user.contrasenia], (err, result) => {
       if (err) {
-        console.error('Error al autenticar el usuario: ' + err.message);
+        console.error("Error al autenticar el usuario: " + err.message);
         callback(err, null);
       } else if (result.length === 0) {
         callback(null, null);
@@ -103,93 +131,105 @@ class userData {
   }
 
   static obtenerGruas(callback) {
-    // const query = 'SELECT * FROM grua WHERE estadoGrua = 1'; 
-    const query = 'SELECT * FROM grua'; 
+    // const query = 'SELECT * FROM grua WHERE estadoGrua = 1';
+    const query = "SELECT * FROM grua";
     database.query(query, (err, result) => {
-        if (err) {
-            console.error('Error al obtener grúas desde la base de datos:', err.message);
-            callback(err, null);
-        } else {
-            callback(null, result);
-        }
+      if (err) {
+        console.error(
+          "Error al obtener grúas desde la base de datos:",
+          err.message
+        );
+        callback(err, null);
+      } else {
+        callback(null, result);
+      }
     });
-}
+  }
 
   static editarGrua(idGrua, nuevosDetalles, callback) {
     console.log(nuevosDetalles);
-    const query = 'UPDATE grua SET marca = ?, modelo = ?, capacidad = ?, whatsapp = ?, ubicacion = ? WHERE idGrua = ?';
-    const values = [nuevosDetalles.marca, nuevosDetalles.modelo, nuevosDetalles.capacidad, nuevosDetalles.whatsapp, nuevosDetalles.ubicacion, idGrua];
+    const query =
+      "UPDATE grua SET marca = ?, modelo = ?, capacidad = ?, whatsapp = ?, ubicacion = ? WHERE idGrua = ?";
+    const values = [
+      nuevosDetalles.marca,
+      nuevosDetalles.modelo,
+      nuevosDetalles.capacidad,
+      nuevosDetalles.whatsapp,
+      nuevosDetalles.ubicacion,
+      idGrua,
+    ];
     console.log("valores de la grua: " + values);
     console.log("id de la grua: " + idGrua);
     database.query(query, values, (err, result) => {
       if (err) {
-        console.error('Error al editar la grúa:', err.message);
+        console.error("Error al editar la grúa:", err.message);
         callback(err, false);
       } else {
-        console.log('Grúa editada exitosamente.');
+        console.log("Grúa editada exitosamente.");
         callback(null, true);
       }
     });
   }
 
   static eliminarGrua(idGrua, callback) {
-    const query = 'DELETE FROM grua WHERE idGrua = ?';
+    const query = "DELETE FROM grua WHERE idGrua = ?";
     database.query(query, [idGrua], (err, result) => {
       if (err) {
-        console.error('Error al eliminar la grúa:', err.message);
+        console.error("Error al eliminar la grúa:", err.message);
         callback(err, false);
       } else {
-        console.log('Grúa eliminada exitosamente.');
+        console.log("Grúa eliminada exitosamente.");
         callback(null, true);
       }
     });
   }
 
   static consultarGruasPorIdCliente(idCliente, callback) {
-    const query = 'SELECT * FROM grua WHERE idCliente = ?';
+    const query = "SELECT * FROM grua WHERE idCliente = ?";
     database.query(query, [idCliente], (err, result) => {
       if (err) {
-        console.error('Error al consultar las grúas por ID de cliente:', err.message);
+        console.error(
+          "Error al consultar las grúas por ID de cliente:",
+          err.message
+        );
         callback(err, null);
       } else {
         callback(null, result);
       }
     });
-}
+  }
 
-static cambiarEstadoGrua(idGrua, nuevoEstado, callback) {
-  // const query = 'UPDATE grua SET estadoGrua = ? WHERE idGrua = ?';
-  const query = 'UPDATE grua SET estadoGrua = ?';
-  const values = [nuevoEstado, idGrua];
-  database.query(query, values, (err, result) => {
+  static cambiarEstadoGrua(idGrua, nuevoEstado, callback) {
+    // const query = 'UPDATE grua SET estadoGrua = ? WHERE idGrua = ?';
+    const query = "UPDATE grua SET estadoGrua = ?";
+    const values = [nuevoEstado, idGrua];
+    database.query(query, values, (err, result) => {
       if (err) {
-          console.error('Error al cambiar el estado de la grúa:', err.message);
-          callback(err, false);
+        console.error("Error al cambiar el estado de la grúa:", err.message);
+        callback(err, false);
       } else {
-          console.log('Estado de la grúa cambiado exitosamente.');
-          callback(null, true);
+        console.log("Estado de la grúa cambiado exitosamente.");
+        callback(null, true);
       }
-  });
-}
+    });
+  }
 
-static obtenerGruasPendientes(callback) {
-  const query = 'SELECT * FROM grua WHERE estadoGrua = 0'; 
-  database.query(query, (err, result) => {
+  static obtenerGruasPendientes(callback) {
+    const query = "SELECT * FROM grua WHERE estadoGrua = 0";
+    database.query(query, (err, result) => {
       if (err) {
-          console.error('Error al obtener grúas desde la base de datos:', err.message);
-          callback(err, null);
+        console.error(
+          "Error al obtener grúas desde la base de datos:",
+          err.message
+        );
+        callback(err, null);
       } else {
-          callback(null, result);
+        callback(null, result);
       }
-  });
+    });
+  }
 }
-
-}
-
-
-
-
 
 module.exports = {
-  userData
+  userData,
 };
